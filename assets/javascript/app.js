2bcf534eb55da0375231937d09e54cf58a4c8919
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-06-08 15:07:11 -0400

    jesus

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,16 @@
-$(document).ready(function () {
-
-console.log('ready1!');
-
-function queryDive() {
-  //var queryURL = "https://api.divesites.com/?mode=detail&siteid=17559"
-  var queryURL = "https://api.divesites.com/?mode=sites&lat=28.5383&lng=81.3792=25";
-  // 28.5383° N, 81.3792° W
-  // http://api.divesites.com/?mode=sites&lat=47.6031537682643&lng=-122.336164712906&dist=25
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-  .done(function(response) {
-    console.log(response.request.loc.area_code);
-    console.log('ready2!)');
-   
-      }); //End .done function
-
-}; //End function queryDive
-
-}); //End Document.ready
+/*$(document).ready(function () {*/
 
 
 
 
+    $.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?tags=florida,scuba&format=json&nojsoncallback=1", function(data){
+        $.each(data.items, function(i,item){
+            $("<img/>").attr("src", item.media.m).appendTo("#images")
+                .wrap("<a href='" + item.link + "'></a>");
+        });
 
-
-
-/* for referencing multiple apis  
-        $.ajaxSetup({
-//      async: false
-    })
-    //Google
-    $.ajax({
-      url: "https:maps.googleapis.com/maps/api/js?key=&callback=initMap",
-      method: "GET"
-    }).done(function(response) {
-      console.log(response);
     });
-    //Amazon
-    $.ajax({
-      url: "",
-      method: "GET"
-    }).done(function(response) {
-      console.log(response);
-    });
-    //Fish 
-    $.ajax({
-      url: "",
-      method: "GET"
-    }).done(function(response) {
-      console.log(response);
-    });
-    //NP and Active Campsite
-    $.ajax({
-      url: "",
-      method: "GET"
-    }).done(function(response) {
-      console.log(response);
-    });
-// ---------------------------------------------------------
 
-    console.log("Because our AJAX requests are asynchronous, this line of code will most likely log first");
-})
-    console.log("ready!");
-*/
+
+/*});*/
+

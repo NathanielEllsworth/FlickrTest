c5b932e0753a87614c3a1d90cba081fc428f11ea
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-06-08 22:39:45 -0400

    Lowered the json callback to 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ console.log("flickr ready!")
     });*/
 
 
-$.getJSON("https://cors-bcs.herokuapp.com" + "/https://api.flickr.com/services/feeds/photos_public.gne?tags=florida,scuba&format=json&nojsoncallback=10", function(data){
+$.getJSON("https://cors-bcs.herokuapp.com" + "/https://api.flickr.com/services/feeds/photos_public.gne?tags=florida,scuba&format=json&nojsoncallback=1", function(data){
     $.each(data.items, function(i,item){
         $("<img/>").attr("src", item.media.m).appendTo("#images")
             .wrap("<a href='" + item.link + "'></a>");
